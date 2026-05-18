66fa13fd0c7a3944efa7433d22944e6dbf58e22c
Shubham Pandey <pandeys2602@gmail.com>
2026-05-18 09:00:45 +0530

    18 may

--- PythonProject1/Assignment-1/Solutions.py
+++ PythonProject1/Assignment-1/Solutions.py
@@ -1,18 +1,18 @@
 # def Reverse_words_order_in_sentence(sentence):
 #     words = sentence.split()
-#     reversed_words = words[::-1]
-#     return ' '.join(reversed_words)
+#     reverse = words[::-1]
+#     return ' '.join(reverse)
 
-# print(Reverse_words_order_in_sentence("python is easy to learn"))
+# print(Reverse_words_order_in_sentence("messi is the goat"))
 
 # def Find_largest_word_in_sentence(sentence):
 #     words= sentence.split()
-#     largest_word = ""
+#     largest = ""
 #     for word in words:
-#         if len(word) > len(largest_word):
-#             largest_word = word
-#     return largest_word
-# print(Find_largest_word_in_sentence("python is easy to learn"))
+#         if len(word) > len(largest):
+#             largest = word
+#     return largest
+# print(Find_largest_word_in_sentence("messi is the goat"))
 
 # def Count_frequency_of_each_word_in_sentence(sentence):
 #     words = sentence.split()
@@ -26,9 +26,31 @@
 
 # print(Count_frequency_of_each_word_in_sentence("messi is the goat messi"))
 
-def Find_common_characters_between_two_strings(s1,s2):
-    for char in s1:
-        if char in s2:
-            print(char, end=" ")
+# def Find_common_characters_between_two_strings(s1,s2):
+#     for char in s1:
+#         if char in s2:
+#             print(char, end=" ")
 
-Find_common_characters_between_two_strings("shubham","pandeyu")        
+# Find_common_characters_between_two_strings("shubham","pandeyu")        
+
+def Second_most_non_repeating_character(s):
+    freq={}
+    for char in s:
+        if char in freq:
+            freq[char] += 1
+        else:
+            freq[char] = 1
+    
+    values = list(freq.values())
+    unique = list(set(values))
+
+    if len(unique)<2:
+        return "No second most non repeating character"
+    
+    unique.sort(reverse=True)
+    second = unique[1]
+    for char in freq:
+        if freq[char] == second:
+            return char
+
+print(Second_most_non_repeating_character("shubham"))
